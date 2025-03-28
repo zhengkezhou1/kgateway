8be0b7cf5d568dbd5430b6a30113b08988ec2ecd
Lawrence G. <lawrence.gadban@solo.io>
2025-03-28 10:50:48 -0500

    correctly set xport socket match on ai cluster (#10961)

--- internal/kgateway/extensions2/plugins/backend/ai/ai_model_cluster.go
+++ internal/kgateway/extensions2/plugins/backend/ai/ai_model_cluster.go
@@ -25,6 +25,12 @@ const (
 	tlsPort = 443
 )
 
+var tls_match = structpb.Struct{
+	Fields: map[string]*structpb.Value{
+		"tls": structpb.NewStringValue("true"),
+	},
+}
+
 func ProcessAIBackend(ctx context.Context, in *v1alpha1.AIBackend, aiSecret *ir.Secret, multiSecrets map[string]*ir.Secret, out *envoy_config_cluster_v3.Cluster) error {
 	if in == nil {
 		return nil
@@ -120,11 +126,11 @@ func buildModelCluster(ctx context.Context, aiUs *v1alpha1.AIBackend, aiSecret *
 		}
 	}
 	// attempt to match tls, the default match is always plaintext
-	tlsMatch := &envoy_tls_v3.UpstreamTlsContext{
+	tlsCtx := &envoy_tls_v3.UpstreamTlsContext{
 		CommonTlsContext: &envoy_tls_v3.CommonTlsContext{},
 		AutoHostSni:      true,
 	}
-	tlsMatchAny, err := utils.MessageToAny(tlsMatch)
+	tlsCtxAny, err := utils.MessageToAny(tlsCtx)
 	if err != nil {
 		return err
 	}
@@ -134,9 +140,10 @@ func buildModelCluster(ctx context.Context, aiUs *v1alpha1.AIBackend, aiSecret *
 			TransportSocket: &envoy_config_core_v3.TransportSocket{
 				Name: wellknown.TransportSocketTls,
 				ConfigType: &envoy_config_core_v3.TransportSocket_TypedConfig{
-					TypedConfig: tlsMatchAny,
+					TypedConfig: tlsCtxAny,
 				},
 			},
+			Match: &tls_match,
 		},
 		{
 			Name: "plaintext",
@@ -306,11 +313,7 @@ func buildLocalityLbEndpoint(
 	}
 	if port == tlsPort {
 		// Used for transport socket matching
-		metadata.GetFilterMetadata()["envoy.transport_socket_match"] = &structpb.Struct{
-			Fields: map[string]*structpb.Value{
-				"tls": structpb.NewStringValue("true"),
-			},
-		}
+		metadata.GetFilterMetadata()["envoy.transport_socket_match"] = &tls_match
 	}
 	return &envoy_config_endpoint_v3.LbEndpoint{
 		Metadata: metadata,
