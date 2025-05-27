9e231f2c9561600c82cae68ba495c54341a52ed9
Shashank Ram <21697719+shashankram@users.noreply.github.com>
2025-05-27 11:12:11 -0700

    policy: move merge types to pluginsdk/policy (#11287)
    Signed-off-by: Shashank Ram <shashank.ram@solo.io>

--- internal/kgateway/extensions2/plugins/trafficpolicy/traffic_policy_plugin.go
+++ internal/kgateway/extensions2/plugins/trafficpolicy/traffic_policy_plugin.go
@@ -43,12 +43,12 @@ import (
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/ir"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/krtcollections"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/plugins"
-	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/policy"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/reports"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/utils"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/wellknown"
 	"github.com/kgateway-dev/kgateway/v2/pkg/client/clientset/versioned"
 	"github.com/kgateway-dev/kgateway/v2/pkg/logging"
+	"github.com/kgateway-dev/kgateway/v2/pkg/pluginsdk/policy"
 )
 
 const (
