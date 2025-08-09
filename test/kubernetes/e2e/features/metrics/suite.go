454dd8eb75dacf5c62122a3cf9b1689938318f3a
David Haifley <dhaifley@users.noreply.github.com>
2025-08-08 20:17:57 -0400

    fix(metrics): update metrics e2e test expected values to reflect policy resource (#11934)
    Signed-off-by: David Haifley <david.haifley@solo.io>

--- test/kubernetes/e2e/features/metrics/suite.go
+++ test/kubernetes/e2e/features/metrics/suite.go
@@ -37,6 +37,9 @@ func (s *testingSuite) checkPodsRunning() {
 	s.TestInstallation.Assertions.EventuallyPodsRunning(s.Ctx, proxyObjectMeta.GetNamespace(), metav1.ListOptions{
 		LabelSelector: "app.kubernetes.io/name=gw1",
 	})
+	s.TestInstallation.Assertions.EventuallyPodsRunning(s.Ctx, proxyObjectMeta.GetNamespace(), metav1.ListOptions{
+		LabelSelector: "app.kubernetes.io/name=gw2",
+	})
 	s.TestInstallation.Assertions.EventuallyPodsRunning(s.Ctx, kgatewayMetricsObjectMeta.GetNamespace(), metav1.ListOptions{
 		LabelSelector: "app.kubernetes.io/name=kgateway",
 	})
@@ -45,6 +48,7 @@ func (s *testingSuite) checkPodsRunning() {
 func (s *testingSuite) TestMetrics() {
 	// Make sure pods are running.
 	s.checkPodsRunning()
+
 	// Verify the test services are created and working.
 	s.TestInstallation.Assertions.AssertEventualCurlResponse(
 		s.Ctx,
@@ -81,14 +85,14 @@ func (s *testingSuite) TestMetrics() {
 				gomega.MatchRegexp(`kgateway_resources_managed\{namespace=\"default\",parent=\"gw2\",resource=\"HTTPRoute\"} 1`),
 				gomega.MatchRegexp(`kgateway_resources_managed\{namespace=\"default\",parent=\"ls1\",resource=\"HTTPRoute\"} 1`),
 				gomega.MatchRegexp(`kgateway_resources_managed\{namespace=\"default\",parent=\"gw2\",resource=\"HTTPListenerPolicy\"} 1`),
-				gomega.MatchRegexp(`kgateway_resources_syncs_started_total\{gateway=\"gw2\",namespace=\"default\",resource=\"Gateway\"} [1-5]`),
-				gomega.MatchRegexp(`kgateway_resources_syncs_started_total\{gateway=\"gw2\",namespace=\"default\",resource=\"HTTPRoute\"} [1-2]`),
+				gomega.MatchRegexp(`kgateway_resources_syncs_started_total\{gateway=\"gw2\",namespace=\"default\",resource=\"Gateway\"} [3-6]`),
+				gomega.MatchRegexp(`kgateway_resources_syncs_started_total\{gateway=\"gw2\",namespace=\"default\",resource=\"HTTPRoute\"} [2-3]`),
 				gomega.MatchRegexp(`kgateway_resources_syncs_started_total\{gateway=\"gw2\",namespace=\"default\",resource=\"HTTPListenerPolicy\"} [1-2]`),
-				gomega.MatchRegexp(`kgateway_resources_status_syncs_completed_total\{gateway=\"gw2\",namespace=\"default\",resource=\"Gateway\"} [1-5]`),
-				gomega.MatchRegexp(`kgateway_resources_status_syncs_completed_total\{gateway=\"gw2\",namespace=\"default\",resource=\"HTTPRoute\"} [1-2]`),
+				gomega.MatchRegexp(`kgateway_resources_status_syncs_completed_total\{gateway=\"gw2\",namespace=\"default\",resource=\"Gateway\"} [3-6]`),
+				gomega.MatchRegexp(`kgateway_resources_status_syncs_completed_total\{gateway=\"gw2\",namespace=\"default\",resource=\"HTTPRoute\"} [2-3]`),
 				gomega.MatchRegexp(`kgateway_resources_status_syncs_completed_total\{gateway=\"gw2\",namespace=\"default\",resource=\"HTTPListenerPolicy\"} [1-2]`),
-				gomega.MatchRegexp(`kgateway_resources_status_sync_duration_seconds_count\{gateway=\"gw2\",namespace=\"default\",resource=\"Gateway\"} [1-5]`),
-				gomega.MatchRegexp(`kgateway_resources_status_sync_duration_seconds_count\{gateway=\"gw2\",namespace=\"default\",resource=\"HTTPRoute\"} [1-2]`),
+				gomega.MatchRegexp(`kgateway_resources_status_sync_duration_seconds_count\{gateway=\"gw2\",namespace=\"default\",resource=\"Gateway\"} [3-6]`),
+				gomega.MatchRegexp(`kgateway_resources_status_sync_duration_seconds_count\{gateway=\"gw2\",namespace=\"default\",resource=\"HTTPRoute\"} [2-3]`),
 				gomega.MatchRegexp(`kgateway_resources_status_sync_duration_seconds_count\{gateway=\"gw2\",namespace=\"default\",resource=\"HTTPListenerPolicy\"} [1-2]`),
 				gomega.MatchRegexp(`kgateway_resources_xds_snapshot_syncs_total\{gateway=\"gw1\",namespace=\"default\",resource=\"Gateway\"} 1`),
 				gomega.MatchRegexp(`kgateway_resources_xds_snapshot_syncs_total\{gateway=\"gw2\",namespace=\"default\",resource=\"Gateway\"} 1`),
